86495dd16afe60561e8f5d1bdf947dbf076a3690
wanku-x <wanku@bk.ru>
2019-01-25 16:41:09 +0300

    Добавлено: все необходимые роуты для получения информации об общежитиях, этажах и комнатах

--- routes/api.php
+++ routes/api.php
@@ -15,15 +15,52 @@ use Illuminate\Support\Facades\Auth;
 */
 
 Route::group(['middleware' => ['web']], function () {
-    Route::get('/login', 'Auth\LoginController@redirectToProvider')->name('login');
-    Route::get('/callback', 'Auth\LoginController@handleProviderCallback')->name('callback');
-    Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
+    Route::prefix('v1')->group(function () {
+        Route::get('/login', 'Auth\LoginController@redirectToProvider')->name('login');
+        Route::get('/callback', 'Auth\LoginController@handleProviderCallback')->name('callback');
+        Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
 
-    Route::prefix('user')->group(function () {
-        Route::get('info', 'UserController@getUserInfo')->name('user_info');
-    });
+        /*
+        | User routes
+        */
 
-    Route::prefix('dorm')->group(function () {
-        Route::get('list', 'DormController@getDormsList')->name('dorm_list');
+        Route::prefix('users')->group(function () {
+            Route::get('current', 'UserController@getCurrentUser')->name('user_current');
+            // only admins for all routes
+            Route::get('/', 'UserController@getUsers')->name('users');
+            Route::get('{id}', 'UserController@getUser')->name('user');
+            Route::get('vk/{id}', 'UserController@getUserByVKId')->name('user_by_vk_id');
+        });
+
+        /*
+        | Dorm routes
+        */
+
+        Route::prefix('dorms')->group(function () {
+            Route::get('/', 'DormController@getDorms')->name('get_dorms');
+            Route::get('{id}', 'DormController@getDorm')->name('get_dorm');
+            Route::get('{id}/floors', 'DormController@getDormFloors')->name('get_dorm_floors');
+            Route::get('{id}/floors/{number}', 'DormController@getDormFloor')->name('get_dorm_floor');
+            Route::get('{id}/floors/{number}/rooms', 'DormController@getDormFloorRooms')->name('get_dorm_floor_rooms');
+        });
+
+        /*
+        | Floor routes
+        */
+
+        Route::prefix('floors')->group(function () {
+            Route::get('/', 'FloorController@getFloors')->name('get_floors');
+            Route::get('{id}', 'FloorController@getFloor')->name('get_floor');
+            Route::get('{id}/rooms', 'FloorController@getFloorRooms')->name('get_floor_rooms');
+        });
+
+        /*
+        | Room routes
+        */
+
+        Route::prefix('rooms')->group(function () {
+            Route::get('/', 'RoomController@getRooms')->name('get_rooms');
+            Route::get('{id}', 'RoomController@getRoom')->name('get_room');
+        });
     });
 });
