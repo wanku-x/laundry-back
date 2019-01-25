4a9ed5553c433895b2b924648b6240e52369d5b9
wanku-x <wanku@bk.ru>
2019-01-25 16:45:55 +0300

    Добавлено: получение всех этажей, этажа по id, комнат этажа по id

--- app/Http/Controllers/FloorController.php
+++ app/Http/Controllers/FloorController.php
@@ -3,8 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Floor;
+use App\Room;
 
 class FloorController extends Controller
 {
-    //
+    public function getFloors()
+    {
+        return Floor::select('floors.*', 'dorms.name as dorm_name')
+            ->leftJoin('dorms', 'floors.dorm_id', '=', 'dorms.id')
+            ->get();
+    }
+
+    public function getFloor($id)
+    {
+        return Floor::select('floors.*', 'dorms.name as dorm_name')
+            ->leftJoin('dorms', 'floors.dorm_id', '=', 'dorms.id')
+            ->find($id);
+    }
+
+    public function getFloorRooms($id)
+    {
+        return Room::select(
+            'rooms.*',
+            'floors.number as floor_number',
+            'floors.dorm_id',
+            'dorms.name as dorm_name'
+        )
+            ->leftJoin('floors', 'rooms.floor_id', '=', 'floors.id')
+            ->leftJoin('dorms', 'floors.dorm_id', '=', 'dorms.id')
+            ->where('floors.id', $id)
+            ->get();
+    }
 }
