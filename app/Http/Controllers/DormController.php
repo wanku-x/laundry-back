3b2f19b0f19d05b03d67df3bacc8a6fca22a6c80
wanku-x <wanku@bk.ru>
2019-01-25 16:44:40 +0300

    Добавлено: получение всех общежитий, общежития по id, этажей общежития по id, этажа общежития по id и number, комнат этажа общежития по id и number

--- app/Http/Controllers/DormController.php
+++ app/Http/Controllers/DormController.php
@@ -5,11 +5,50 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Dorm;
+use App\Floor;
+use App\Room;
 
 class DormController extends Controller
 {
-    public function getDormsList()
+    public function getDorms()
     {
         return Dorm::all();
+    }
+
+    public function getDorm($id)
+    {
+        return Dorm::find($id);
+    }
+
+    public function getDormFloors($id)
+    {
+        return Floor::select('floors.*', 'dorms.name as dorm_name')
+            ->leftJoin('dorms', 'floors.dorm_id', '=', 'dorms.id')
+            ->where('floors.dorm_id', $id)
+            ->get();
+    }
+
+    public function getDormFloor($id, $number)
+    {
+        return Floor::select('floors.*', 'dorms.name as dorm_name')
+            ->leftJoin('dorms', 'floors.dorm_id', '=', 'dorms.id')
+            ->where('floors.dorm_id', $id)
+            ->where('floors.number', $number)
+            ->get();
+    }
+
+    public function getDormFloorRooms($id, $number)
+    {
+        return Room::select(
+                'rooms.*',
+                'floors.number as floor_number',
+                'floors.dorm_id',
+                'dorms.name as dorm_name'
+            )
+            ->leftJoin('floors', 'rooms.floor_id', '=', 'floors.id')
+            ->leftJoin('dorms', 'floors.dorm_id', '=', 'dorms.id')
+            ->where('floors.dorm_id', $id)
+            ->where('floors.number', $number)
+            ->get();
     }
 }
