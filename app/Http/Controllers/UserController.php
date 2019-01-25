1e90692242cb715b0fb2bc27f49edf14e0355620
wanku-x <wanku@bk.ru>
2019-01-25 16:42:30 +0300

    Добавлено: получение текущего пользователя, всех пользователей, пользователя по id, пользователя по vk_id

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use App\User;
 
 class UserController extends Controller
 {
-    public function getUserInfo()
+    public function getCurrentUser()
     {
         $user = Auth::user();
         if (!$user) {
@@ -17,5 +18,23 @@ class UserController extends Controller
         }
         $user->auth = true;
         return $user;
+    }
+
+    public function getUsers()
+    {
+        // only admin
+        return User::all();
+    }
+
+    public function getUser($id)
+    {
+        // only admin
+        return User::find($id);
+    }
+
+    public function getUserByVKId($vk_id)
+    {
+        // only admin
+        return User::where('vk_id', $vk_id)->first();
     }
 }
