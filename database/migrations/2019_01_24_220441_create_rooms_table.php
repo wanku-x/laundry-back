8080291c2e04fa51a06dfefed9ed0b8bf4e96ffe
wanku-x <wanku@bk.ru>
2019-01-25 16:38:52 +0300

    Добавлено: миграция для модели Room

---
+++ database/migrations/2019_01_24_220441_create_rooms_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('floor_id')->nullable();
+            $table->foreign('floor_id')
+                  ->references('id')
+                  ->on('floors')
+                  ->onDelete('cascade');
+            $table->string('number');
+            $table->unique(['floor_id', 'number']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
